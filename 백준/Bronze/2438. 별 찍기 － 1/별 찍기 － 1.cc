1670c04272b1e9ac2669c402c2c864052df73d67
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2022-08-21 22:54:02 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int i, j, num;
+
+	scanf("%d", &num);
+	
+	for (i = 1; i <= num; i++) {
+		for(j = 1; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+}
