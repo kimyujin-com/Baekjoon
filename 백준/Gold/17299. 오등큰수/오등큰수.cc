f33e5a43ef6eab062872b2027b30e806778bddaa
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2023-01-22 17:07:46 +0900

    [Gold III] Title: 오등큰수, Time: 372 ms, Memory: 16624 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.cc
+++ 백준/Gold/17299. 오등큰수/오등큰수.cc
@@ -1,32 +1,88 @@
-#include <cstdio>
-#include <stack>
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
 
-int n;
-int F[1000001], ans[1000001], nums[1000001];
-stack<int> st;
-int main() {
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &nums[i]);
-		F[nums[i]]++;
+#define MAX_STACK_SIZE 1000010
+
+typedef int element;
+typedef struct {
+	int top;
+	element data[MAX_STACK_SIZE];
+}StackType;
+
+void init_stack(StackType* s)
+{
+	s->top = -1;
+}
+
+int is_full(StackType* s)
+{
+	return s->top == MAX_STACK_SIZE;
+}
+
+int is_empty(StackType* s)
+{
+	return s->top == -1;
+}
+
+void push(StackType* t, element item)
+{
+	if (is_full(t))
+		return;
+	t->data[++t->top] = item;
+}
+
+element pop(StackType* s)
+{
+	if (is_empty(s))
+		return -1;
+	return s->data[s->top--];
+}
+
+element peek(StackType* s)
+{
+	if (is_empty(s))
+		return -1;
+	return s->data[s->top];
+
+}
+
+int main()
+{
+	int i, N;
+	int tmp[MAX_STACK_SIZE];
+	int F[MAX_STACK_SIZE];
+	int NGF[MAX_STACK_SIZE];
+	StackType s;
+
+	scanf("%d", &N);
+
+	//tmp = (int*)malloc(sizeof(int) * N);
+	//F = (int*)malloc(sizeof(int) * N);
+	for (i = 0; i < N; i++) {
+		scanf("%d", &tmp[i]);
+		F[tmp[i]]++;
 	}
 
-	for (int i = n - 1; i >= 0; i--) {
-		int number = nums[i];
-		int height = F[number];
-		while (!st.empty()) {
-			int topNum = nums[st.top()];
+	init_stack(&s);
+	//s.data = (int*)malloc(sizeof(int) * N);
+	//NGF = (int*)malloc(sizeof(int) * N);
+	for (i = N - 1; i >= 0; i--) {
+		int num = tmp[i];
+		int height = F[num];
+		while (!is_empty(&s)) {
+			int topNum = tmp[peek(&s)];
 			int topHeight = F[topNum];
-			if (height >= topHeight) st.pop();
+			if (topHeight <= height)
+				pop(&s);
 			else break;
 		}
-		ans[i] = -1;
-		if (!st.empty()) ans[i] = nums[st.top()];
-		st.push(i);
+		NGF[i] = -1;
+		if (!is_empty(&s)) NGF[i] = tmp[peek(&s)];
+		push(&s, i);
 	}
-	for (int i = 0; i < n; i++)
-		printf("%d ", ans[i]);
-	printf("\n");
 
+	for (i = 0; i < N; i++)
+		printf("%d ", NGF[i]);
+	printf("\n");
 }
