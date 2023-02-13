11fda9b0e1f8547750049ce571c0d23cfcb19809
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2023-02-13 19:56:53 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 56 ms, Memory: 3376 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,104 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_VERTICES 10001
+#define INF 1000
+
+int parent[MAX_VERTICES];
+
+void set_init(int n)
+{
+	for (int i = 1; i <= n; i++)
+		parent[i] = i;
+}
+int set_find(int curr)
+{
+	if (parent[curr] == curr) return curr;
+	//부모의 값에다 부모의 부모를 넣어라
+	else return parent[curr] = set_find(parent[curr]);
+}
+
+void set_union(int a, int b)
+{
+	int root1 = set_find(a);
+	int root2 = set_find(b);
+	if (root1 != root2)
+		parent[root1] = root2;
+}
+
+struct Edge {
+	int start, end, weight;
+};
+typedef struct GraphType {
+	int n;//간선의 개수
+	int nvertex;//정점의 개수
+	struct Edge edges[10 * MAX_VERTICES];
+}GraphType;
+
+void graph_init(GraphType* g)
+{
+	g->n = g->nvertex = 0;
+	for (int i = 0; i < 10 * MAX_VERTICES; i++) {
+		g->edges[i].start = 0;
+		g->edges[i].end = 0;
+		g->edges[i].weight = INF;
+	}
+}
+
+void insert_edge(GraphType* g, int start, int end, int w)
+{
+	g->edges[g->n].start = start;
+	g->edges[g->n].end = end;
+	g->edges[g->n].weight = w;
+	g->n++;
+}
+
+int compare(const void* first, const void* second)
+{
+	if (((struct Edge*)first)->weight > ((struct Edge*)second)->weight)
+		return 1;
+	else if (((struct Edge*)first)->weight < ((struct Edge*)second)->weight)
+		return -1;
+	else
+		return 0;
+}
+
+int kruskal(GraphType* g)
+{
+	int edge_accepted = 0;
+	int i = 0, sum = 0;
+	int uset, vset;
+	struct Edge e;
+
+	set_init(g->nvertex);
+	qsort(g->edges, g->n, sizeof(struct Edge), compare);
+	while (edge_accepted < (g->nvertex - 1))
+	{
+		e = g->edges[i++];
+		uset = set_find(e.start);
+		vset = set_find(e.end);
+		if (uset != vset) {
+			edge_accepted++;
+			set_union(uset, vset);
+			sum += e.weight;
+		}
+	}
+	return sum;
+}
+
+int main(void)
+{
+	int V, E;
+	int a, b, c;
+	int i;
+	GraphType g;
+
+	graph_init(&g);
+	scanf("%d %d", &V, &E);
+	g.nvertex = V;
+	for (i = 0; i < E; i++) {
+		scanf("%d %d %d", &a, &b, &c);
+		insert_edge(&g, a, b, c);
+	}
+	printf("%d\n", kruskal(&g));
+}
