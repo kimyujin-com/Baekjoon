91122f7e2f60cf7a93742695dfe1225c70089a00
김유진 <80165026+kimyujin-com@users.noreply.github.com>
2023-12-11 02:30:50 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,47 +1,25 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
 
-int pick(int* items, int itemSize, int* bucket, int bucketSize, int k, int S)
-{
-	int i, lastIndex, smallest, sum = 0, cnt = 0;
+int n, s;
+int arr[30];
+int cnt = 0;
 
-	if (k == 0) {
-		for (i = 0; i < bucketSize; i++)
-			sum += items[bucket[i]];
-		if (sum == S)
-			return 1;
+void func(int cur, int tot) {
+	if (cur == n) {
+		if (tot == s) cnt++;
+		return;
 	}
-
-	if (k == 0) 
-		return cnt;
-
-	lastIndex = bucketSize - k - 1;
-	if (k == bucketSize)
-		smallest = 0;
-	else
-		smallest = bucket[lastIndex] + 1; // 중복되지 않도록
-
-	for (i = smallest; i < itemSize; i++) {
-		bucket[lastIndex + 1] = i;
-		cnt += pick(items, itemSize, bucket, bucketSize, k - 1, S);
-	}
-	return cnt;
+	func(cur + 1, tot);
+	func(cur + 1, tot + arr[cur]);
 }
 
-int main()
-{
-	int *items;
-	int* bucket;
-	int i, N, S, cnt = 0;
-
-	scanf("%d", &N);
-	scanf("%d", &S);
-	items = (int*)malloc(sizeof(int) * N);
-	for (i = 0; i < N; i++)
-		scanf("%d", &items[i]);
-	bucket = (int*)malloc(sizeof(int) * N);
-	for (i = 1; i <= N; i++)
-		cnt += pick(items, N, bucket, i, i, S);
-	printf("%d\n", cnt);
+int main() {
+	scanf("%d %d", &n, &s);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	func(0, 0);
+	if (s == 0) cnt--;
+	printf("%d", cnt);
+	return 0;
 }
